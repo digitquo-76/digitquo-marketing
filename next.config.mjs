7df9d071e2b36c925205eb9cc57d908d6e12ddc0
esekh7557-cloud <esekh7557@gmail.com>
2026-07-11 15:10:09 +0530

    Fix syntax error in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const nextConfig = {
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
     ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
